900906d38606dcf39494f2a52687ea6d9e308cb7
MarquitoYutoba <marcosmori2004@gmail.com>
2025-11-20 14:14:43 -0300

    Update produto.py

--- backend/rotas/produto.py
+++ backend/rotas/produto.py
@@ -6,9 +6,11 @@ produtos_blueprint = Blueprint("produto", __name__)
 
 @produtos_blueprint.route("/produtos", methods=["GET"])
 def getprodutos():
+    nome = request.args.get("nome", "")
     origem = request.args.get("origem", "")
     loja = request.args.get("loja", "")
     categoria = request.args.get("categoria", "")
     preco_min = request.args.get("preco_min", "")
     preco_max = request.args.get("preco_max", "")
-    return jsonify(ProdutoDatabase().get_produto(origem, loja, categoria, preco_min, preco_max)), 200
+    bem_avaliado = request.args.get("bem_avaliado", "")
+    return jsonify(ProdutoDatabase().get_produto(nome, origem, loja, categoria, preco_min, preco_max, bem_avaliado)), 200
--- backend/servicos/produto.py
+++ backend/servicos/produto.py
@@ -5,8 +5,8 @@ class ProdutoDatabase:
     def __init__(self, db_provider=DatabaseManager()) -> None:
         self.db = db_provider
 
-    # Função para consultar os produtos com filtros de origem, loja, categoria e intervalos de preço
-    def get_produto(self, origem: str, loja: str, categoria: str, min_p: str, max_p: str):
+    # Função para consultar os produtos com filtros de nome, origem, loja, categoria, intervalos de preço, bem avaliado
+    def get_produto(self, nome: str, origem: str, loja: str, categoria: str, min_p: str, max_p: str, bem_avaliado: str):
         query = """
                 SELECT 
                     p.alerta_estoque, 
@@ -16,39 +16,52 @@ class ProdutoDatabase:
                     v.nome_loja, 
                     p.nome_produto, 
                     p.origem, 
-                    p.preco 
+                    p.preco,
+                    ROUND(AVG(a.nota), 2) AS media_nota
                 FROM produto p
                 LEFT JOIN vendeprod vp ON vp.id_produto = p.id_produto
                 LEFT JOIN pertencecat pc ON pc.id_produto = p.id_produto
                 LEFT JOIN categoria cat ON cat.id_categoria = pc.id_categoria
                 LEFT JOIN vendedor v ON v.cpf_vendedor = vp.cpf_vendedor
+                LEFT JOIN avaliaprod a ON a.id_produto = p.id_produto
                 """
 
+        where_clauses = []
+
+        if nome:
+            where_clauses.append(f"p.nome_produto LIKE '%{nome}%'")
+
         if origem:
-            query += f"WHERE p.origem = '{origem}'\n"
+            where_clauses.append(f"p.origem = '{origem}'")
 
         if loja:
-            if "WHERE" in query:
-                query += f"AND v.nome_loja = '{loja}'\n"
-            else:
-                query += f"WHERE v.nome_loja = '{loja}'\n"
+            where_clauses.append(f"v.nome_loja = '{loja}'")
 
         if categoria:
-            if "WHERE" in query:
-                query += f"AND cat.nome_categoria = '{categoria}'\n"
-            else:
-                query += f"WHERE cat.nome_categoria = '{categoria}'\n"
+            where_clauses.append(f"cat.nome_categoria = '{categoria}'")
 
         if min_p:
-            if "WHERE" in query:
-                query += f"AND p.preco >= {min_p}\n"
-            else:
-                query += f"WHERE p.preco >= {min_p}\n"
+            where_clauses.append(f"p.preco >= {min_p}")
 
         if max_p:
-            if "WHERE" in query:
-                query += f"AND p.preco <= {max_p}\n"
-            else:
-                query += f"WHERE p.preco <= {max_p}\n"
+            where_clauses.append(f"p.preco <= {max_p}")
+
+        if where_clauses:
+            query += "WHERE " + " AND ".join(where_clauses) + "\n"
+
+        query += """
+                GROUP BY
+                    p.alerta_estoque, 
+                    p.desc_produto, p.estoque_atual, 
+                    p.id_produto, 
+                    cat.nome_categoria, 
+                    v.nome_loja, 
+                    p.nome_produto, 
+                    p.origem, 
+                    p.preco
+                """
+
+        if bem_avaliado:
+            query += "\nHAVING AVG(a.nota) >= 4"
 
         return self.db.execute_select_all(query)
